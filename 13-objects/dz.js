8395962a7482c2d2a63e341af259b4cae0bfb7ac
malyutin.na <malyutin.na@gazprom-neft.ru>
2025-10-07 15:50:11 +0300

    Query params

---
+++ 13-objects/dz.js
@@ -0,0 +1,26 @@
+/*	Написать функцию, которая принимает объект query параметров и возвращает строку для вставки:
+	{
+		search: ‘Вася’,
+		take: 10,
+	}
+	search=Вася&take=10
+*/
+
+const queryParams = {
+    search: 'Вася',
+    take: 10,
+};
+
+function getQueryParams(queryParams) {
+	let searchString = '';
+	for (const key in queryParams) {
+		if (searchString != '') {
+			searchString += '&';
+		}
+		searchString += `${key}=${queryParams[key]}`;
+	}
+
+	return searchString;
+}
+
+console.log(getQueryParams(queryParams));
